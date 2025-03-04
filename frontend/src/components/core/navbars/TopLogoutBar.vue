04b25c41ec00ce22157ff19be793742230c27731
maple-chen-tw <yongxuanchen0106@gmail.com>
2025-03-04 16:11:35 +0800

    Update TopLogoutBar.vue

--- frontend/src/components/core/navbars/TopLogoutBar.vue
+++ frontend/src/components/core/navbars/TopLogoutBar.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>LOGOUT</div>
+    <div class="flex flex-col space-x-2 justify-around 
+        sticky top-0 bg-black border-gray-800 border-t border-b">
+      <div class="xl:block flex justify-end font-sans text-md font-normal text-white">
+        <button @click="logout" class="text-lg px-4 py-2 hover:bg-slate-700 rounded-sm">Logout</button>
+      </div>
+    </div>
   </template>
   
-  <script>
-  export default {
-    name: "TopLogoutBar",
-    // component logic
-  };
+<script setup lang="ts">
+import { onMounted, defineComponent, computed, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
+const router = useRouter()
+const toast = useToast()
+
+const logout = () => {
+    // TODO: Add logout logic and make a universal logout function
+    toast.success('Logout successful')
+    //   Sleep for 2 seconds
+    setTimeout(() => {
+        router.push('/accounts/login')
+    }, 2000)
+
+}
   </script>
   
