3f354cb5f42ba05cddb14862e32e3f985ad8fcda
FDSmaplechen <yongxuanchen0106@gmail.com>
2025-06-08 16:48:42 +0800

    上雲調整:前端env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(), 
+    removeConsole()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    'process.env': {
+      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
+      VITE_SOCKET_URL: JSON.stringify(process.env.VITE_SOCKET_URL),
     }
   }
 })
